935809cd4e940b5aaf42af6c11e015ac1df53f74
ameserole <ameserole@gmail.com>
2016-09-19 12:24:30 -0500

    Fill in Data Base Header File
    Added member functions and data to the database.h header file

--- DataBase.h
+++ DataBase.h
@@ -1,6 +1,8 @@
 #include <unordered_map>
 #include "Table.h"
 #include <string>
+#include <vector>
+#include <pair>
 
 using namespace std;
 
@@ -11,5 +13,11 @@ class DataBase
 	
 public:
 
-	void createTable()
+	void createTable(vector<pair<string, int>>, vector<string>);
+	void dropTable(string tableName);
+	Table setUnion(string tableName1, string tableName2);
+	Table setDifference(string tableName1, string tableName2);
+	Table cossProduct(string tableName1, string tableName2);
+	Table getTable(string tableName);
+	void readTableFromDisk(string fileName);
 }
