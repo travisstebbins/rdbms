9c466e2904b251b6666b4e9a769a331d1c39c17a
Travis Stebbins <stebbins.travis@compute-linux1.cs.tamu.edu>
2016-09-20 22:33:10 -0500

    Update Table.h and add makefile

--- Table.h
+++ Table.h
@@ -7,17 +7,12 @@ using namespace std;
 
 class Table
 {
-<<<<<<< HEAD
 	string name;
 	unordered_map<string, tuple<> > tableHashMap;
 	vector<pair<string, int> > attributes;
-=======
-	unordered_map<string, tuple<>> tableHashMap;
 	
 public:
 	//left empty so that database would compile
 	Table() {}
 	Table(string tableName, vector<pair<string, int>> attributes, vector<string> primaryKeys) {}
-
->>>>>>> bb5f7eb578404e3bab09f4e322a243fb009e8c1e
 };
