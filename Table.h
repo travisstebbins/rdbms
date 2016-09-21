96d6ad67c4429f90d21f6cd1a90cd736fb63073b
Travis Stebbins <stebbins.travis@compute-linux1.cs.tamu.edu>
2016-09-21 00:10:22 -0500

    Add private evaluate function and private insertRecord function that takes a tuple to Table class header file

--- Table.h
+++ Table.h
@@ -11,19 +11,23 @@ class Table
 	unordered_map<string, tuple<> > data;
 	vector<pair<string, int> > attributes;
 	vector<string> primaryKeys;
+
+	// private functions
+	bool evaluate(tuple<> entry, vector<string> boolExpressions);
+	void insertRecord(tuple<> entry);
 	
 public:
 	// constructors
 	Table() {}
 	Table(string _name, vector<pair<string, int>> _attributes, vector<string> _primaryKeys);
 	// data manipulation functions
-	Table select(vector<string> boolExpressions);
-	Table project(vector<string> desiredAttributes);
-	Table rename(vector<string> newNames);
-	void show();
+	Table select(string _name, vector<string> boolExpressions);
+	Table project(string _name, vector<string> desiredAttributes);
+	Table rename(string _name, vector<string> newNames);
+	string show();
 	void insertRecord(vector<string> entry);
 	void insertRecord(Table relationship);
 	void deleteRecord(vector<string> boolExpressions);
-	void updateRecord(vecto<string> attributes, vector<string> values, vector<string> boolExpressions);
+	void updateRecord(vector<string> attributes, vector<string> values, vector<string> boolExpressions);
 	void writeToDisk();
 };
