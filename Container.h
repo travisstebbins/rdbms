e2ddd8cbf99ddf141cc96c6cb3a09d698e29b65d
ameserole <ameserole@gmail.com>
2016-09-28 11:59:30 -0500

    Test file addition attempt

--- Container.h
+++ Container.h
@@ -26,17 +26,22 @@ class Container {
 		void setVarchar(varchar vc) { data._varchar = vc; }
 		void setInt(int i) { data._int = i; }
 
-		void operator=(const Container &c);
+		Container& operator=(const Container &c);
+		
+		// inline bool operator==(const Container rhs) const
+		// {
+			// return true;//(this->data._varchar == rhs.data._varchar) || (this->data._int == rhs.data._int);
+		// }
 
 	private:
 		union Data {
 			varchar _varchar;
 			int _int;
 
-			Data() { new(&_varchar) varchar(0); }
+			Data() { new(&_varchar) varchar(0); _int = 0; }
 			~Data() {}
 		} data;
 
 		Type _type;
-
+		
 };
--- Varchar.h
+++ Varchar.h
@@ -10,8 +10,23 @@ class varchar
 	int limit;
 
 	public:
+		varchar() {data = "t"; limit = 0;}
 		varchar(int _limit);
 		~varchar() {}
 		string getString() { return data; }
 		void setString(string s);
+		string getData() const { return data;}
+		int getLimit() const {return limit;}
+		// varchar& operator=(const varchar& other)
+		// {
+			// if(&other == this)
+				// return *this;
+			// this->data = other.getData();
+			// this->limit = other.getLimit();
+			// return *this;
+		// }
+		// inline bool operator==(const varchar& rhs) const
+		// {
+			// return (this->data == rhs.data);
+		// }
 };
