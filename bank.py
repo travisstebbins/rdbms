de0f74a1991b845d21dcd9f5253b0b9aee45d50c
lyan515 <lyan515@tamu.edu>
2016-10-04 21:42:12 -0500

    Create bank.py
    Creates our python file for the Interactive system. I also included some simple stuff like our ASCII art.

---
+++ bank.py
@@ -0,0 +1,24 @@
+import operator
+import sys
+
+exit = 0
+print "   _____                 .__         __________                __    "
+print "  /  _  \    ____   ____ |__| ____   \______   \_____    ____ |  | __"
+print " /  /_\  \  / ___\ / ___\|  |/ __ \   |    |  _/\__  \  /    \|  |/ /"
+print "/    |    \/ /_/  > /_/  >  \  ___/   |    |   \ / __ \|   |  \    < "
+print "\____|__  /\___  /\___  /|__|\___  >  |______  /(____  /___|  /__|_ |"
+print "        \//_____//_____/         \/          \/      \/     \/     \/"
+print "\nWelcome to Aggie Bank!"
+while exit == 0:
+	print "\n enter one of the following commands:"
+	print "1.)"
+	print "2.)"
+	print "3.)"
+	print "4.)"
+	print "5.)"
+	print ""
+	print ""
+	tst = raw_input('>')
+	
+
+	
