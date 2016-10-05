d0bd46d7d9be5f5c7723dba2e60be263ae2cc2be
ameserole <ameserole@gmail.com>
2016-10-05 10:55:16 -0500

    Add Command Line Arguments For Engine

--- main.cpp
+++ main.cpp
@@ -6,12 +6,27 @@
 
 using namespace std;
 
-int main()
+int main(int argc, char **argv)
 {
 	try
 	{
 		Parser test;
-		test.runOnSocket();
+		int c;
+		while ((c = getopt (argc, argv, "cs")) != -1)
+			switch(c)
+			{
+				case 'c':
+					test.runOnCommandLine();
+					break;
+				case 's':
+					test.runOnSocket();
+					break;
+				case '?':
+					cout << "Usage: " << argv[0] << " -c (Run on command Line) or -s (Run as server)" << endl;
+			}
+		if (argv[optind] == NULL)
+			cout << "Usage: " << argv[0] << " -c (Run on command Line) or -s (Run as server)" << endl;	
+			
 	}
 	catch (char const* c)
 	{
