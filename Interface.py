790396138722e93ab7d8fef76c5419b25ccdb22a
ameserole <ameserole@gmail.com>
2016-10-06 16:52:53 -0500

    Clean up Interface

--- Interface.py
+++ Interface.py
@@ -18,11 +18,7 @@ print "Add User  <Account Number> <Account Name>"
 print "Delete User <Account Number>"
 print "Update Balance <Account Number> <Account(Savings or Checking)> <Money Amount>" 
 print "Display User <Account Number>"
-<<<<<<< HEAD
 print "Tansfer Money <Account Number> <Account(Savings or Checking)> <Money Amount>" #what about the target account?
-=======
-print "Tansfer Money <Account Number> <Account(Savings or Checking)> <Money Amount>"
->>>>>>> master
 print "Exit"
 
 error_message = "We didn't quite understand that. Please check your inputs and try again."
@@ -30,7 +26,6 @@ error_message = "We didn't quite understand that. Please check your inputs and t
 #TODO: add socket functionality
 #TODO: comment out all pass statements as the functions are implemented
 
-<<<<<<< HEAD
 #TODO: make read_in() return something after each inputted line
 
 def read_in():
@@ -82,38 +77,6 @@ def read_in():
 	else:
 		print error_message
 
-=======
-#TODO: make read_in() return something aftet each inputted line
-def read_in(): 
-    while (exit == 0):
-	#for line in sys.stdin:
-		instruction = raw_input() #not necessary, but I think it looks nicer
-		inputsplit = line.split(' ') #splits the input string by whitespace
-		if(inputsplit[0] == "Add"):
-			#Add User <Account Number> <Account Name>
-			#modify and send instruction socket to parser
-			instruction =  "INSERT INTO Users" + inputsplit[2] + ";" #other stuff
-			pass #tells the compiler to "pass by" without doing anything
-		elif(inputsplit[0] == "Delete"):
-			#modify and send instruction socket to parser
-			instruction = "DELETE FROM Users WHERE accountNumber == " + inputsplit[2]
-			pass
-		elif(inputsplit[0] == "Update"):
-			#modify and send instruction socket to parser
-			pass
-		elif(inputsplit[0] == "Display"):
-			#modify and send instruction socket to parser
-			pass
-		elif(inputsplit[0] == "Transfer"):
-			#modify and send instruction socket to parser
-			pass
-		elif(inputsplit[0] == "Exit"):
-			instruction = "EXIT;"
-			pass
-		else:
-			print error_message
-
->>>>>>> master
 initialInstruction = "CREATE TABLE Users (accountNumber INTEGER, accountName VARCHAR(30), savings INTEGER, checking INTEGER) PRIMARY KEY (accountName);"
 #TODO: send this initial instruction
 read_in()
