8c6daa9a8ff6a82ee17e18efb6b937f79328d0d6
Ryan Branson <lyan515@compute-linux1.cs.tamu.edu>
2016-10-07 13:41:26 -0500

    Created startup and main functions. also some bug fixing

--- Interface.py
+++ Interface.py
@@ -1,55 +1,28 @@
 import socket
 import sys
 import socket
-#TODO: figure out how to "include" the c++ parser, if we even need to do that at all
-exit = 0
 
 SERVERADDRESS = 'localhost'
 PORT = 1337
 
-print "      _                      _            ____                    _      " 
-print "     / \      __ _    __ _  (_)   ___    | __ )    __ _   _ __   | | __  "
-print "    / _ \    / _` |  / _` | | |  / _ \   |  _ \   / _` | | '_ \  | |/ /  " 
-print "   / ___ \  | (_| | | (_| | | | |  __/   | |_) | | (_| | | | | | |   <   "
-print "  /_/   \_\  \__, |  \__, | |_|  \___|   |____/   \__,_| |_| |_| |_|\_\  "
-print "             |___/   |___/                                               "
-
-print " "
-print "Welcome to Aggie Bank"
-print "Enter a command from the following list of commands."
-print "Be sure to enter the right parameters!"
-print "--------------------------------------------------------------"
-print "Add User  <Account Number> <Account Name>"
-print "Delete User <Account Number>"
-print "Update Balance <Account Number> <Account(Savings or Checking)> <Money Amount>" 
-print "Display User <Account Number>"
-print "Transfer Money <Account Number> <Account(Savings or Checking)> <Money Amount>" #what about the target account?
-print "Exit"
-
+exit = 0
 error_message = "We didn't quite understand that. Please check your inputs and try again."
 
-#TODO: add socket functionality
 #TODO: comment out all pass statements as the functions are implemented
 
-#TODO: make read_in() return something after each inputted line
-
 def read_in():
-	instruction = raw_input()
-	inputsplit = line.split(' ') #splits the input string by whitespace
+	instruction = raw_input(">")
+	inputsplit = instruction.split(' ') #splits the input string by whitespace
 	if(inputsplit[0] == "Add"):
 		#modify and send instruction socket to parser
 		instr = 'INSERT INTO Users VALUES FROM (' + inputsplit[2] + ', ' + inputsplit[3] + ', 0, 0);' 
-		#					  (account number,        account name,   savings, checking)
-
-		pass #tells the compiler to "pass by" and ignore without doing anything
-		     #with pass, you can work on a function without having to worry as much about unfinished code
-		     #preventing you from compiling
+		#(account number, account name,savings, checking)
+		sendToSocket(instr)
+		
 	elif(inputsplit[0] == "Delete"):
 		#modify and send instruction socket to parser
-
 		instr = 'DELETE FROM Users WHERE (accountNumber == ' + inputsplit[2] + ');'
-
-		pass
+		sendToSocket(instr)
 	elif(inputsplit[0] == "Update"):
 		#modify and send instructions socket to parser
 		#inst1 = 'currentbal <- project (' + inputsplit[3] ') (select (accountNumber == ' + inputsplit[2] + ') Users);' #TODO: how to access this number?
@@ -68,9 +41,9 @@ def read_in():
 		pass
 	elif(inputsplit[0] == "Display"):
 		#modify and send instructions socket to parser
-		instr1 = 'thisUser <- select(accountNumber == ' + inputsplit[2] ') Users;'	#INSERT is the only command that can handle query inputs
+		instr1 = 'thisUser <- select(accountNumber == ' + inputsplit[2] + ') Users;'	#INSERT is the only command that can handle query inputs
 		instr2 = 'SHOW thisUser;'							#splitting Display into these three instructions gets around this issue
-		instr3 = 'DROP TABLE thisUser;' #allows us to reuse thisUser			#without us having to restructure a significant amount of our code
+		instr3 = 'DROP TABLE thisUser;' #allows us to reuse thisUser without us having to restructure a significant amount of our code
 
 		pass
 	elif(inputsplit[0] == "Transfer"):
@@ -78,16 +51,11 @@ def read_in():
 		pass
 	elif(inputsplit[0] == "Exit"):
 		instr = 'EXIT;'
+		exit = 1
 		pass
 	else:
 		print error_message
-
-
-initialInstruction = "CREATE TABLE Users (accountNumber INTEGER, accountName VARCHAR(30), savings INTEGER, checking INTEGER) PRIMARY KEY (accountName);"
-#TODO: send this initial instruction
-read_in()
-
-#read_in()
+	sendToSocket(instr)
 
 def sendToSocket(msg):
 	sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
@@ -96,7 +64,38 @@ def sendToSocket(msg):
 	sock.connect(serverAddress)
 	sock.send(msg)
 	retMsg = sock.recv(1024)
-	print "Recieved: " + retMsg
+	print "Received: " + retMsg
 	sock.close()
 	return retMsg
+
+def startup():
+	print "      _                      _            ____                    _      " 
+	print "     / \      __ _    __ _  (_)   ___    | __ )    __ _   _ __   | | __  "
+	print "    / _ \    / _` |  / _` | | |  / _ \   |  _ \   / _` | | '_ \  | |/ /  " 
+	print "   / ___ \  | (_| | | (_| | | | |  __/   | |_) | | (_| | | | | | |   <   "
+	print "  /_/   \_\  \__, |  \__, | |_|  \___|   |____/   \__,_| |_| |_| |_|\_\  "
+	print "             |___/   |___/                                               "
+
+	print " "
+	print "Welcome to Aggie Bank"
 	
+	instr = "CREATE TABLE bank (name VARCHAR(20), account# INTEGER, savings INTEGER, checking INTEGER) PRIMARY KEY (name, account#);"
+	sendToSocket(instr)
+	
+def main():
+	startup()
+	
+	
+	while exit == 0:
+		print "Enter a command from the following list of commands."
+		print "Be sure to enter the right parameters!"
+		print "--------------------------------------------------------------"
+		print "Add User  <Account Number> <Account Name>"
+		print "Delete User <Account Number>"
+		print "Update Balance <Account Number> <Account(Savings or Checking)> <Money Amount>" 
+		print "Display User <Account Number>"
+		print "Transfer Money <Account Number> <Account(Savings or Checking)> <Money Amount>" #what about the target account?
+		print "Exit"
+		read_in()
+
+main()
