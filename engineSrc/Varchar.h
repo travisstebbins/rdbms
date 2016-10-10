f07287b85f80801e6023dee9bd2271c40ecaa621
ameserole <ameserole@gmail.com>
2016-10-09 22:30:32 -0500

    Update engine tests to work with pointers

--- engineSrc/Varchar.h
+++ engineSrc/Varchar.h
@@ -16,14 +16,7 @@ class varchar
 		void setString(string s);
 		string getData() const { return data;}
 		int getLimit() const {return limit;}
-		// varchar& operator=(const varchar& other)
-		// {
-			// if(&other == this)
-				// return *this;
-			// this->data = other.getData();
-			// this->limit = other.getLimit();
-			// return *this;
-		// }
+
 		inline bool operator==(const varchar& rhs) const
 		{
 			return (this->data == rhs.data);
